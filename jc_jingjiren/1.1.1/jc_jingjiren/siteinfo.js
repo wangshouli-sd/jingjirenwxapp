97d8441e1de7681d01edecd15b914ec9d459c69c
wangshouli <wangshoulixx@163.com>
2018-12-29 11:34:48 +0800

    更新

--- jc_jingjiren/1.1.1/jc_jingjiren/siteinfo.js
+++ jc_jingjiren/1.1.1/jc_jingjiren/siteinfo.js
@@ -1,14 +1,12 @@
 var siteinfo = {
-    "uniacid": "4",
-    "acid": "3",
-    "multiid": "0",
-    "version": "v1.0",
+  "uniacid": "4",
+  "acid": "3",
+  "multiid": "0",
+  "version": "v1.0",
   "siteroot": "http://weiqing-wsl.com/app/index.php",
-    'method_design': '3 '
+  'method_design': '3 '
 };
-module.exports = siteinfo;
-
-
+module.exports = siteinfo
 // var siteinfo = {
 //     "uniacid": "56",
 //     "acid": "56",
